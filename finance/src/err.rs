09d3df0ddb54070c820b43ca77e106167bec3519
Alex Kouprin <hello@kikimora.tech>
2022-04-27 23:20:48 +0100

    finance: remove unused

--- finance/src/err.rs
+++ finance/src/err.rs
@@ -1,5 +1,7 @@
 use crate::*;
 
+use near_sdk::FunctionError;
+
 #[derive(BorshDeserialize, BorshSerialize, Serialize, PartialEq, Debug)]
 #[cfg_attr(not(target_arch = "wasm32"), derive(Deserialize))]
 #[serde(crate = "near_sdk::serde")]
@@ -9,4 +11,10 @@ pub enum ContractError {
         expected: AccountId,
         received: AccountId,
     },
+}
+
+impl FunctionError for ContractError {
+    fn panic(&self) -> ! {
+        crate::env::panic_str(&format!("{:?}", self))
+    }
 }
